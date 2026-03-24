dcde47c7226289917f294ccd462e2b7d33e2a8fd
Luis Henrique Vieira de Oliveira <luis.henrique@lacreisaude.com.br>
2026-03-24 11:44:15 -0300

    feat: implementa cálculos de densidade populacional e pib per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,6 +15,7 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
+    float densidade1, pibPerCapita1;
 
     // Declaração de Variáveis para a Carta 2
     char estado2;
@@ -24,6 +25,7 @@ int main() {
     float area2;
     float pib2;
     int pontosTuristicos2;
+    float densidade2, pibPerCapita2;
 
 
   // Área para entrada de dados
@@ -76,6 +78,16 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
+  // Área para cálculos (densidade populacional e PIB per capita)
+
+  // Cálculos para a Carta 1
+  densidade1 = (float)populacao1 / area1;
+  pibPerCapita1 = (pib1 * 1000000000.0) / (float)populacao1; // Multiplicado por 1 bi para converter o PIB
+
+  // Cálculos para a Carta 2
+  densidade2 = (float)populacao2 / area2;
+  pibPerCapita2 = (pib2 * 1000000000.0) / (float)populacao2;
+
   // Área para exibição dos dados da cidade
 
     printf("\n-------------------------------\n");
@@ -87,6 +99,8 @@ int main() {
     printf("Área: %.2f km²\n", area1); // %.2f limita a duas casas decimais
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
@@ -96,6 +110,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
 
 
 return 0;
